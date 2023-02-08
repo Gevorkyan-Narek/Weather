502f57375d12fdc678064ddb7a1c1edd842df4df
Gevorkyan-Narek <narek.gevorkyan@magenta-technology.com>
2023-02-08 11:12:38 +0400

    remove redundant

--- app/src/main/java/com/weather/weather/modules/PlatformModule.kt
+++ app/src/main/java/com/weather/weather/modules/PlatformModule.kt
@@ -2,8 +2,6 @@ package com.weather.weather.modules
 
 import com.weather.core.data.api.ConnectionManager
 import com.weather.core.data.impl.connection.ConnectionManagerImpl
-import com.weather.core.domain.api.ConnectionUseCase
-import com.weather.core.domain.impl.ConnectionUseCaseImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -13,9 +11,4 @@ val platformModule = module {
         ConnectionManagerImpl(androidContext())
     }
 
-    single<ConnectionUseCase> {
-        ConnectionUseCaseImpl(
-            connectionManager = get()
-        )
-    }
 }
